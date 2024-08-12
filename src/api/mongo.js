625ad7654c8b17b6cf5303e6c03e308f6bae4c57
cardgraph22 <129220366+cardgraph22@users.noreply.github.com>
2024-08-12 02:20:54 -0700

    Fix env for vercel

--- src/api/mongo.js
+++ src/api/mongo.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose"
 mongoose.set('strictQuery', false);
 
-import { MONGODB_URI } from process.env.MONGODB_URI;
+//import { MONGODB_URI } from process.env.MONGODB_URI;
+
+let MONGODB_URI = process.env.MONGODB_URI;
 
 export async function startMongo() {
   console.log('mongo.js, connect to db')
